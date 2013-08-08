ab84311df5b98919773232ac829b1d6a5edef3f4
Carlos Crespo <carlos.crespog@gmail.com>
2013-08-08 04:53:09 +0200

    first commit

---
+++ dni.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+var=$(ssh bifrost.gsi.dit.upm.es tail -n 2 /opt/doorcontrol/log/door.log)
+IFS=' ' read -a array <<< "$var"
+#array=$(echo $var | tr " " "\n")
+stream="<stream-element timestamp='"
+stream="$stream${array[1]}"
+IFS=',' read -a subarray <<< "${array[2]}"
+stream="$stream ${subarray[0]}"
+stream="$stream CEST'>"
+stream="$stream <field name='number' type='numeric'>"
+dniNumber=${array[${#array[@]}-1]}
+stream="$stream$dniNumber"
+stream="$stream</field><field name='name' type='string'>"
+stream="$stream${array[6]} ${array[7]}"
+stream="$stream</field></stream-element>"
+echo "$stream">xmlDni.xml
+curl -v -s -X PUT -d "notification-id=1.3" --data-urlencode data@xmlDni.xml http://192.168.1.115:22001/streaming/
+
+
+#echo $var
+#echo $stream
+
---
+++ light.py
@@ -0,0 +1,75 @@
+#!/usr/local/bin/python
+
+
+import RPi.GPIO as GPIO, time
+import urllib2
+import urllib
+import datetime
+# Tell the GPIO library to use
+# Broadcom GPIO references
+GPIO.setmode(GPIO.BCM)
+
+# Define function to measure charge time
+def RCtime (PiPin):
+  measurement = 0
+  # Discharge capacitor
+  GPIO.setup(PiPin, GPIO.OUT)
+  GPIO.output(PiPin, GPIO.LOW)
+  time.sleep(0.1)
+
+  GPIO.setup(PiPin, GPIO.IN)
+  # Count loops until voltage across
+  # capacitor reads high on GPIO
+  while (GPIO.input(PiPin) == GPIO.HIGH):
+	time.sleep(0.1)
+  while (GPIO.input(PiPin) == GPIO.LOW):
+    measurement += 1
+
+  return measurement
+
+# Main program loop
+while True:
+  gsnserver='http://192.168.1.115:22001/streaming'
+  print RCtime(17) # Measure timing using GPIO17
+  stream=''
+  stream+='<stream-element timestamp=\''
+  now=datetime.datetime.now()
+  dateNow=now.date()
+  y = dateNow.year
+  m = dateNow.month
+  d = dateNow.day
+  timeNow=now.time()
+  H = timeNow.hour
+  M = timeNow.minute
+  S = timeNow.second
+  stream+=str(y)
+  stream+='-'
+  stream+=str(m)
+  stream+='-'
+  stream+=str(d)
+  stream+=' '
+  stream+=str(H)
+  stream+=':'
+  stream+=str(M)
+  stream+=':'
+  stream+=str(S)
+  stream+=' UTC\'>'
+  stream+='<field name=\'value\' type=\'numeric\'>'
+  stream+= str(RCtime(17))
+  stream+='</field></stream-element>'
+  print stream
+  params = {}
+  opener = urllib2.build_opener(urllib2.HTTPHandler)
+
+  params['notification-id']=1.2
+  params['data'] = stream
+  params = urllib.urlencode(params) 
+  request = urllib2.Request(gsnserver, data=params)
+  request.add_header('Content-Type','application/x-www-form-urlencoded')
+  opener = urllib2.build_opener(urllib2.HTTPHandler)
+  request.get_method = lambda: 'PUT'
+  url = opener.open(request)
+#  params = urllib.urlencode(params)
+ # f = urllib.urlopen("http://192.168.1.116:3000/light", params)
+  time.sleep(1)
+  
